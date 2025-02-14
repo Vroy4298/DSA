ac2436b91b05affafc3dff01b3408b23156dea96
VIVEK KUMAR <143505810+Vroy4298@users.noreply.github.com>
2025-02-14 20:33:08 +0530

    Time: 0 ms (100%), Space: 9.9 MB (57.79%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+#include <cctype>  
+#include <string>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) {
+                left++;  
+            }
+            while (left < right && !isalnum(s[right])) {
+                right--; 
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;  
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
