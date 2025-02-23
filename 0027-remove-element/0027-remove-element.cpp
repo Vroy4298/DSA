79b2830ebba14e3a0a406296829eb0713925eaf9
VIVEK KUMAR <143505810+Vroy4298@users.noreply.github.com>
2025-02-24 00:30:31 +0530

    Time: 0 ms (100%), Space: 11.6 MB (74.96%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,16 +1,14 @@
-#include <vector>
-using namespace std;
 
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int i = 0; // Slow pointer
+        int i = 0; 
         for (int j = 0; j < nums.size(); j++) {
             if (nums[j] != val) {
-                nums[i] = nums[j]; // Move non-val elements to the front
+                nums[i] = nums[j]; 
                 i++;
             }
         }
-        return i; // Number of elements not equal to val
+        return i; 
     }
 };
