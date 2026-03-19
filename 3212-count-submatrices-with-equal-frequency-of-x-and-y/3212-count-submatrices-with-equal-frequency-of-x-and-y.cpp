08eca009890cce008b921a993075eefc0a913173
VIVEK KUMAR <143505810+Vroy4298@users.noreply.github.com>
2026-03-19 20:09:50 +0530

    Time: 483 ms (5.69%), Space: 284.5 MB (5.69%) - LeetHub

---
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+      
+        vector<vector<vector<int>>> prefixSum(rows + 1, 
+                                              vector<vector<int>>(cols + 1, 
+                                              vector<int>(2, 0)));
+      
+        int result = 0;
+     
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= cols; ++j) {
+              
+                prefixSum[i][j][0] = prefixSum[i - 1][j][0] 
+                                    + prefixSum[i][j - 1][0] 
+                                    - prefixSum[i - 1][j - 1][0]
+                                    + (grid[i - 1][j - 1] == 'X' ? 1 : 0);
+                prefixSum[i][j][1] = prefixSum[i - 1][j][1] 
+                                    + prefixSum[i][j - 1][1] 
+                                    - prefixSum[i - 1][j - 1][1]
+                                    + (grid[i - 1][j - 1] == 'Y' ? 1 : 0);
+                if (prefixSum[i][j][0] > 0 && prefixSum[i][j][0] == prefixSum[i][j][1]) {
+                    ++result;
+                }
+            }
+        }
+      
+        return result;
+    }
+};
